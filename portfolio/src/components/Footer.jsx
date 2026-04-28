30cac4425c0d8714237042f3034a6f4e8a1c44c4
Brenda Anievski <135037211+brendaanievskimts@users.noreply.github.com>
2026-04-28 02:32:16 +0000

    feat: restructure portfolio components and assets
    - Added new components: Navbar, Hero, About, Experience, Projects, Skills, Contact, and Footer.
    - Replaced hero image with a new image (brenda_img.png) and removed the old hero.png.
    - Added new logo assets: logo-br-claro.svg and logo-br-escuro.svg.
    - Removed unused SVG assets: react.svg and vite.svg.
    - Created new JSON files for internationalization (i18n) in English and Portuguese for common terms, curriculum, home, projects, and skills.
    - Implemented initial structure for Curriculum, Home, Projects, and Skills pages.

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+    return (
+        <div className="hero">
+            <h1 class="fu d2">
+                Olá, eu sou
+                <em>Brenda.</em>
+            </h1> 
+        </div>
+    )
+}
+
+export default Hero
